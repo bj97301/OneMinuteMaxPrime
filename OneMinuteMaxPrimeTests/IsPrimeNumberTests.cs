1460a718800f9000f613995df68056b7f2d06b91
Bryan Joseph <bj97301@gmail.com>
2016-06-15 21:44:33 -0700

    Add IsPrimeNumberTests file

---
+++ OneMinuteMaxPrimeTests/IsPrimeNumberTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace OneMinuteMaxPrimeTests
+{
+    [TestFixture]
+    internal class IsPrimeNumberTests
+    {
+        [Test]
+        public void CanRecognizeKnownPrimeNumbers()
+        {
+        }
+    }
+}
