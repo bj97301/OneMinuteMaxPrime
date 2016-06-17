362ccf79403e49abff40f2b331c45b4edfa5bf8c
Bryan Joseph <bj97301@gmail.com>
2016-06-16 22:20:36 -0700

    Add largest prime number calculator action and tests
    LargestPrimeNumberCalculatorAction will run for a given period of time
    while finding the largest prime number

---
+++ OneMinuteMaxPrime/LargestPrimeNumberCalculatorAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace OneMinuteMaxPrime
+{
+    public class LargestPrimeNumberCalculatorAction
+    {
+        public static int Execute(int milliseconds)
+        {
+            var currentLargestPrimeNumber = 3;
+            var s = new Stopwatch();
+            s.Start();
+            while (s.Elapsed <= TimeSpan.FromMilliseconds(milliseconds))
+            {
+            }
+            s.Stop();
+
+            return currentLargestPrimeNumber;
+        }
+    }
+}
---
+++ OneMinuteMaxPrimeTests/LargestPrimeNumberCalculatorAction.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Compatibility;
+using OneMinuteMaxPrime;
+
+namespace OneMinuteMaxPrimeTests
+{
+    [TestFixture]
+    public class LargestPrimeNumberCalculatorActionTests
+    {
+        [Test]
+        public void CanRunForGivenAmountOfTime()
+        {
+            var timer = new Stopwatch();
+            const int expectedExecuteionTime = 1;
+            timer.Start();
+
+            LargestPrimeNumberCalculatorAction.Execute(expectedExecuteionTime);
+            timer.Stop();
+
+            timer.Elapsed.Milliseconds.ShouldBeEquivalentTo(expectedExecuteionTime);
+        }
+    }
+}
