3bfb87e8af1ec4cc5fa3cdeebf8197b8ceb72f2e
Bryan Joseph <bj97301@gmail.com>
2016-06-17 18:32:35 -0700

    Update console interface to interact with methods
    Obtaining execution time from user

--- OneMinuteMaxPrime/Program.cs
+++ OneMinuteMaxPrime/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace OneMinuteMaxPrime
+﻿using System;
+
+namespace OneMinuteMaxPrime
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Input the number of seconds the program will run then press enter/return.");
+            var seconds = Convert.ToInt32(Console.ReadLine());
+            
+            LargestPrimeNumberCalculatorAction.Execute((int)TimeSpan.FromSeconds(seconds).TotalMilliseconds);
+            Console.ReadLine();
         }
     }
 }
