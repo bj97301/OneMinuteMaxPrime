82d1916f35dec84c4cf3aa61e21edef0d6fd78dc
Bryan Joseph <bj97301@gmail.com>
2016-06-17 18:42:10 -0700

    update program.cs to send the logger parameter

--- OneMinuteMaxPrime/Program.cs
+++ OneMinuteMaxPrime/Program.cs
@@ -8,8 +8,9 @@ namespace OneMinuteMaxPrime
         {
             Console.WriteLine("Input the number of seconds the program will run then press enter/return.");
             var seconds = Convert.ToInt32(Console.ReadLine());
-            
-            LargestPrimeNumberCalculatorAction.Execute((int)TimeSpan.FromSeconds(seconds).TotalMilliseconds);
+
+            LargestPrimeNumberCalculatorAction.Execute((int) TimeSpan.FromSeconds(seconds).TotalMilliseconds,
+                Console.Write);
             Console.ReadLine();
         }
     }
